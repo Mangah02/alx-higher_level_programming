35ef6830664d19d60aacc3ecd2617bca83535c59
Joel Manga <jmanga86@gmail.com>
2022-05-31 20:48:02 +0300

    print all possible combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-for num in range(0, 90):
-        if num % 10 > num / 10:
-                if num != 89:
-                        print("{:02d}, ".format(num), end='')
-                else:
-                        print("{:02d}".format(num))
+
+
+def print_uniq_combos():
+    for i in range(10):
+        for j in range(i + 1, 10):
+            if i == 8 and j == 9:
+                print('{:d}{:d}'.format(i, j))
+            else:
+                print('{:d}{:d}'.format(i, j), end=', ')
+
+
+if __name__ == '__main__':
+    print_uniq_combos()
