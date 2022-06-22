397af0bd4c384ecc04bb6424dda495753cac12c4
Joel Manga <jmanga86@gmail.com>
2022-06-22 03:02:40 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""Check type"""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """Private instance attribute: size
+    Instantiation with optional """
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
-        if not isinstance(size, int):
+        """Initializes attribute size """
+
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = int(size)
+            self.__size = size
