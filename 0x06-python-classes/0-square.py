6ba691a858be5fad53c8048086669aed1eca7876
Joel Manga <jmanga86@gmail.com>
2022-06-22 03:00:36 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Filename:  0-square"""
+
+
 class Square:
-    """Class Square that defines a square.
-    """
+    """An empty class Square that defines a square:"""
     pass
