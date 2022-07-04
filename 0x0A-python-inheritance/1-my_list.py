e8a455a8f91b48e9e022c68f2949d10ca2d0be31
Joel Manga <jmanga86@gmail.com>
2022-07-04 21:26:46 +0300

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+'''
+A class MyList that inherits from list
+'''
+
+
 class MyList(list):
-    """ Class that inherits the attributes references of class list
-    Args:
-        list: class list
-    """
+    '''
+    MyList class
+    '''
 
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        print(sorted(self))
