37fa67d67cd39e97c88afa48bc38554d4768c934
Joel Manga <jmanga86@gmail.com>
2022-07-05 14:17:58 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list, and then
-save them to a file
-"""
+import os
 import sys
-import os.path
+import json
 
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
+filename = 'add_item.json'
+args = len(sys.argv)
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
+if not os.path.isfile(filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('[]')
 
-save_file(my_list, "add_item.json")
+if args > 1:
+    data = load_from_json_file(filename)
+    for i in range(1, args):
+        data.append(sys.argv[i])
+    save_to_json_file(data, filename)
